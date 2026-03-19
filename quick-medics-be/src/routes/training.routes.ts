b6722e0b885344b78e853f3c73e73816e0ef8383
Gbotemi <hommzmum@gmail.com>
2026-03-19 14:30:19 +0100

    updated

--- quick-medics-be/src/routes/training.routes.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/training.routes.ts
-import { Router } from 'express';
-import { submitApplication } from '../controllers/training.controller';
-
-const router = Router();
-
-router.post('/apply', submitApplication);
-
-export default router;
